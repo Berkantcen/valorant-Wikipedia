e00d136117d220202a8b3a94de697330dc6d059f
Cem Bıçakcı <73403359+cembicakci@users.noreply.github.com>
2022-07-11 19:14:09 +0300

    updated AppLevelContext.js

--- src/context/AppLevelContext.js
+++ src/context/AppLevelContext.js
@@ -1,35 +1,53 @@
-import React from 'react'
-import axios from 'axios'
-import { useState, useEffect } from 'react'
+import React, { createContext, useState } from "react";
+import axios from "axios";
 
-const AppLevelContext = React.createContext()
-const API = 'https://valorant-api.com/v1'
+const AppLevelContext = createContext();
+
+const API = 'https://valorant-api.com/v1';
 
 export const AppLevelProvider = ({ children }) => {
-  const [agents, setAgents] = useState([])
-  const [maps, setMaps] = useState([])
-  const [weapons, setWeapons] = useState([])
 
-  const fetchAgents = async () => {
-    const response = await axios.get(`${API}/agents?isPlayableCharacter=true`)
-    setAgents(response.data.data)
-  }
+    const [agents, setAgents] = useState([]);
+    const [maps, setMaps] = useState([]);
+    const [weapons, setWeapons] = useState([]);
 
-  return (
-    <AppLevelContext.Provider
-      value={{
-        agents,
-        maps,
-        weapons,
-        fetchAgents,
-        setMaps,
-        setWeapons,
-        setAgents,
-      }}
-    >
-      {children}
-    </AppLevelContext.Provider>
-  )
+    const fetchAgents = async () => {
+        const response = await axios.get(`${API}/agents?isPlayableCharacter=true`)
+        setAgents(response.data.data)
+    }
+
+    const fetchWeapons = async () => {
+        const response = await axios.get(`${API}/weapons`)
+        setWeapons(response.data.data)
+    }
+
+    const fetchMaps = async () => {
+        const response = await axios.get(`${API}/maps`)
+        setMaps(response.data.data)
+    }
+
+
+    return (
+        <AppLevelContext.Provider value={{
+            agents,
+            setAgents,
+            maps,
+            setMaps,
+            weapons,
+            setWeapons,
+            fetchAgents,
+            fetchWeapons,
+            fetchMaps
+
+        }}>
+
+            {children}
+
+        </AppLevelContext.Provider>
+    )
 }
 
+
 export default AppLevelContext
+
+
